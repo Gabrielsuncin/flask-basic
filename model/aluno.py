4845423e82f756fd08e5d5420060548e8aaba165
Gabrielsuncin <gc.everit@itmss.com.br>
2021-01-19 18:11:21 -0300

    updating study project

---
+++ model/aluno.py
@@ -0,0 +1,33 @@
+class Aluno():
+    def __init__(self, id, nome, matricula):
+        self.id = id
+        self.nome = nome
+        self.matricula = matricula
+
+    def atualizar(self, dados):
+        try:
+            id = dados["id"]
+            nome = dados["nome"]
+            self.id, self.nome = id, nome
+            return self
+        except Exception as e:
+            print("Problema ao criar novo aluno!")
+            print(e)
+
+    def __dict__(self):
+        d = dict()
+        d['id'] = self.id
+        d['nome'] = self.nome
+        d['matricula'] = self.matricula
+        return d
+
+    @staticmethod
+    def criar(dados):
+        try:
+            id = dados["id"]
+            nome = dados["nome"]
+            matricula = dados["matricula"]
+            return Aluno(id=id, nome=nome, matricula=matricula)
+        except Exception as e:
+            print("Problema ao criar novo aluno!")
+            print(e)
